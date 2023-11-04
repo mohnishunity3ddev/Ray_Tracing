bb62f8a0ace17934271a098770f5e341deb29290
Mohnish Sharma <mohnish.unity3d.dev@gmail.com>
2023-11-04 16:48:13 +0530

    A List of Hittable Objects

---
+++ common/HittableList.h
@@ -0,0 +1,46 @@
+#if !defined(HITTABLE_LIST_H)
+#include "defines.h"
+#include "Hittable.h"
+
+#include <memory>
+#include <vector>
+
+class hittable_list : public hittable
+{
+  public:
+    std::vector<std::shared_ptr<hittable>> Objects;
+    
+    hittable_list() {}
+    hittable_list(std::shared_ptr<hittable> Object) { add(Object); }
+    
+    void clear() { Objects.clear(); }
+    
+    void add(std::shared_ptr<hittable> Object)
+    {
+        Objects.push_back(Object);
+    }
+    
+    bool
+    Hit(const ray &Ray, f64 Ray_TMin, f64 Ray_TMax,
+        hit_record &Record) const override
+    {
+        hit_record TempRecord;
+        b32 HitAnything = false;
+        f64 ClosestSoFar = Ray_TMax;
+        
+        for(const auto &Object : Objects)
+        {
+            if(Object->Hit(Ray, Ray_TMin, Ray_TMax, TempRecord))
+            {
+                HitAnything = true;
+                ClosestSoFar = TempRecord.t;
+                Record = TempRecord;
+            }
+        }
+
+        return HitAnything;
+    }
+};
+
+#define HITTABLE_LIST_H
+#endif
